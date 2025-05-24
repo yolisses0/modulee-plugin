7078ea7c3ae75c8fc0e3a9d997775161da631758
Yolisses <yolisses0@gmail.com>
2025-05-24 09:47:39 -0300

    Open Google sign in the default browser

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -117,6 +117,20 @@ ModuleeAudioProcessorEditor::ModuleeAudioProcessorEditor(
   // editor's size to whatever you need it to be.
   setSize(800, 600);
   setResizable(true, true);
+
+  juce::String clientId = "725523345294-l7ljv04v2maac7k6ugu6ifmuut88gbjk.apps."
+                          "googleusercontent.com";
+  // TODO replace by custom URI like "com.myapp://oauth"
+  juce::String redirectUri = "http://localhost:5173/signIn/response";
+  juce::String scope = "https://www.googleapis.com/auth/userinfo.profile "
+                       "https://www.googleapis.com/auth/userinfo.email";
+  juce::String authUrl = "https://accounts.google.com/o/oauth2/v2/auth?"
+                         "client_id=" +
+                         clientId + "&redirect_uri=" + redirectUri +
+                         "&response_type=code"
+                         "&scope=" +
+                         scope + "&access_type=offline";
+  juce::URL(authUrl).launchInDefaultBrowser();
 }
 
 ModuleeAudioProcessorEditor::~ModuleeAudioProcessorEditor() {}
