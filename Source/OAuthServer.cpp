ac2237e4adfdbdb727e163b7da1d6cce7f32c0c6
Yolisses <yolisses0@gmail.com>
2025-05-24 12:04:38 -0300

    Get credential in OAuthServer

--- Source/OAuthServer.cpp
+++ Source/OAuthServer.cpp
@@ -7,12 +7,16 @@ OAuthServer::~OAuthServer() { stop(); }
 void OAuthServer::run() {
   server = std::make_unique<httplib::Server>();
 
-  // Handle GET request to any path
-  server->Get("/.*", [](const httplib::Request &, httplib::Response &res) {
+  server->Get("/signIn",
+              [](const httplib::Request &req, httplib::Response &res) {
+                std::string credential = req.get_param_value("code");
+                res.set_content(credential, "text/plain");
+              });
+
+  server->Get("/ping", [](const httplib::Request &, httplib::Response &res) {
     res.set_content("pong", "text/plain");
   });
 
-  // Start server on localhost:39057
   juce::Logger::writeToLog("Starting server on localhost:39057");
   if (!server->listen("127.0.0.1", 39057)) {
     juce::Logger::writeToLog("Failed to start server on localhost:39057");
@@ -21,13 +25,10 @@ void OAuthServer::run() {
 
 void OAuthServer::stop() {
   if (server) {
-    // Explicitly stop the server
     server->stop();
     server.reset();
     juce::Logger::writeToLog("OAuthServer stopped.");
   }
-  // Signal the thread to exit
   signalThreadShouldExit();
-  // Wait for the thread to exit (with a timeout to avoid hanging)
   waitForThreadToExit(5000); // Wait up to 5 seconds
 }
