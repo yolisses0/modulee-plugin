a912b03222198c425ad42df06666a7352c438f0b
Yolisses <yolisses0@gmail.com>
2025-05-31 11:05:34 -0300

    Fix TokenManager class declaration duplication

--- Source/TokenManager.cpp
+++ Source/TokenManager.cpp
@@ -1,30 +1,23 @@
 #include "TokenManager.h"
 
-class TokenManager {
-public:
-  TokenManager() {
-    tokenFile =
-        juce::File::getSpecialLocation(juce::File::userApplicationDataDirectory)
-            .getChildFile("Modulee/token");
-    tokenFile.getParentDirectory().createDirectory();
-  }
+TokenManager::TokenManager() {
+  tokenFile =
+      juce::File::getSpecialLocation(juce::File::userApplicationDataDirectory)
+          .getChildFile("Modulee/token");
+  tokenFile.getParentDirectory().createDirectory();
+}
 
-  void saveToken(const juce::String &token) {
-    juce::ScopedLock lock(tokenMutex);
-    tokenFile.replaceWithText(token);
-  }
+void TokenManager::saveToken(const juce::String &token) {
+  juce::ScopedLock lock(tokenMutex);
+  tokenFile.replaceWithText(token);
+}
 
-  std::optional<juce::String> getToken() const {
-    juce::ScopedLock lock(tokenMutex);
-    if (tokenFile.existsAsFile()) {
-      auto token = tokenFile.loadFileAsString().trim();
-      if (!token.isEmpty())
-        return token;
-    }
-    return std::nullopt;
+std::optional<juce::String> TokenManager::getToken() const {
+  juce::ScopedLock lock(tokenMutex);
+  if (tokenFile.existsAsFile()) {
+    auto token = tokenFile.loadFileAsString().trim();
+    if (!token.isEmpty())
+      return token;
   }
-
-private:
-  juce::File tokenFile;
-  juce::CriticalSection tokenMutex;
-};
+  return std::nullopt;
+}
