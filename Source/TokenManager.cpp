1201c0681578661cc548b6df7a9ef77577f12d88
Yolisses <yolisses0@gmail.com>
2025-05-31 10:37:49 -0300

    Create TokenManager

---
+++ Source/TokenManager.cpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <JuceHeader.h>
+
+class TokenManager {
+public:
+  TokenManager() {
+    tokenFile =
+        juce::File::getSpecialLocation(juce::File::userApplicationDataDirectory)
+            .getChildFile("Modulee/token");
+    tokenFile.getParentDirectory().createDirectory();
+  }
+
+  void saveToken(const juce::String &token) {
+    juce::ScopedLock lock(tokenMutex);
+    tokenFile.replaceWithText(token);
+  }
+
+  std::optional<juce::String> getToken() const {
+    juce::ScopedLock lock(tokenMutex);
+    if (tokenFile.existsAsFile()) {
+      auto token = tokenFile.loadFileAsString().trim();
+      if (!token.isEmpty())
+        return token;
+    }
+    return std::nullopt;
+  }
+
+private:
+  juce::File tokenFile;
+  juce::CriticalSection tokenMutex;
+};
