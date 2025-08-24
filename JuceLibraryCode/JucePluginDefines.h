d9b646bb3fe081d28d60233c3b46f20890459b96
Yolisses <yolisses0@gmail.com>
2025-08-24 20:37:03 -0300

    Fix release build in Windows

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -155,7 +155,7 @@
  #define JucePlugin_ARAFactoryID           "com.Yolisses.Modulee.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.Yolisses.Modulee.aradocumentarchive.1.0.0"
+ #define JucePlugin_ARADocumentArchiveID   "com.Yolisses.Modulee.aradocumentarchive.0.0.1"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
